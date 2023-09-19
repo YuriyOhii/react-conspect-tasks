40e02eb9f446af076d8bf0f3079399bf85ed8046
YuriyOhii <uaogiy@gmail.com>
2023-09-19 14:37:59 +0300

    adds try/catch module

--- src/App.js
+++ src/App.js
@@ -6,17 +6,31 @@ axios.defaults.baseURL = "https://hn.algolia.com/api/v1";
 export class App extends Component {
   state = {
     articles: [],
+    isLoading: false,
+    error: null,
   };
 
   async componentDidMount() {
-    const response = await axios.get("search?query=react");
-    this.setState({articles: response.data.hits});
+    try {
+      this.setState({ isLoading: true });
+      const response = await axios.get("search?qury=react");
+      this.setState({ articles: response.data.hits });
+    } catch(error) {
+      this.setState({ error });
+    } finally {
+      this.setState({isLoading: false});
+    }
   }
 
   render() {
-    const { articles } = this.state;
+    const { articles, isLoading, error } = this.state;
     return (
-      <>{articles.length > 0 ? <ArticlesList articles={articles} /> : <p>Nothing</p>}</>
+      <>
+      {error && <p>Whoopss... something went wrong!</p>}
+        {isLoading && <p>Loading...</p>}
+
+        {articles.length > 0 ? <ArticlesList articles={articles} /> : null}
+      </>
     );
   }
 }
